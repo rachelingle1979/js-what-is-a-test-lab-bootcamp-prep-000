43e2feb0f7165cc76165ec5454ac5aff06daacab
Rachel Ingle <rachelpost79@yahoo.com>
2018-01-23 21:53:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,4 +3,6 @@ var name = "Susan";
 var height = 35;
 var message = `I think ${name} is super short because ${height} is abonormally small.`;
 
+console.log(message);
+
 module.exports = {name, height, message};
